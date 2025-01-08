628a3e32f52f82c145181508bbf1a02ee6443460
ohmranger <ohmranger@gmail.com>
2025-01-08 13:58:52 +0700

    Update install_ros_melodic.sh

--- install_ros_melodic.sh
+++ install_ros_melodic.sh
@@ -26,22 +26,11 @@ echo "[Install build environment, the chrony, ntpdate and set the ntpdate]"
 sudo apt install -y chrony ntpdate curl build-essential
 sudo ntpdate ntp.ubuntu.com
 echo "[Add the ROS repository]"
-if [ ! -e /etc/apt/sources.list.d/ros-latest.list ]; then
-  sudo sh -c "echo \"deb http://packages.ros.org/ros/ubuntu ${name_os_version} main\" > /etc/apt/sources.list.d/ros-latest.list"
-fi
-echo "[Download the ROS keys]"
-roskey=`apt-key list | grep "Open Robotics"`
-if [ -z "$roskey" ]; then
-  curl -s https://raw.githubusercontent.com/.../rosd.../master/ros.asc | sudo apt-key add -
-fi
-echo "[Check the ROS keys]"
-roskey=`apt-key list | grep "Open Robotics"`
-if [ -n "$roskey" ]; then
-  echo "[ROS key exists in the list]"
-else
-  echo "[Failed to receive the ROS key, aborts the installation]"
-  exit 0
-fi
+
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt install curl # if you haven't already installed curl
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add - 
+
 echo "[Update the package lists]"
 sudo apt update -y
 echo "[Install ros-desktop-full version of $name_ros_version"
